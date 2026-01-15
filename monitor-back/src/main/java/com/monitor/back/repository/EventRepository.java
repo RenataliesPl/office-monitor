a7fde89928a87fe587c58eef90e8dac45d5aa25c
Renata Plawska <gax35670@student.gdynia.merito.pl>
2026-01-15 10:57:16 +0100

    Backend extensions

--- monitor-back/src/main/java/com/monitor/back/repository/EventRepository.java
+++ monitor-back/src/main/java/com/monitor/back/repository/EventRepository.java
@@ -2,9 +2,10 @@ package com.monitor.back.repository;
 
 import com.monitor.back.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findTop10ByOrderByTimestampDesc();
+    List<Event> findAllBySensorOrderByTimestampDesc(String sensor);
 }
--- monitor-back/src/main/java/com/monitor/back/repository/StatusRepository.java
+++ monitor-back/src/main/java/com/monitor/back/repository/StatusRepository.java
@@ -2,8 +2,12 @@ package com.monitor.back.repository;
 
 import com.monitor.back.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface StatusRepository extends JpaRepository<Status, String> {
     Optional<Status> findById(String id);
+
+    List<Status> findAllById(String id);
 }
